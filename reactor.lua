c1da95e0089f53a433a67f1117914823c61711fc
agentflemme <48907859+agentflemme@users.noreply.github.com>
2019-03-25 10:25:19 +0100

    Update reactor.lua

--- reactor.lua
+++ reactor.lua
@@ -1,1 +1,17 @@
+--les variables--
+reactor = requires ("reactor")
+function.reactor.stored = requires ("function.reactor.stored")
+function.reactor.full = requires ("function.reactor.full")
+function.reactor.storage = requires ("function.reactor.storage")
+function.reactor.activated = requires ("function.reactor.activated")
+% = function.reactor.stored * function.reactor.full / function.reactor.storage
 
+--le code--
+
+while true do
+  
+if % == 20 and function.reactor.activated == false
+    then
+    
+  function.reactor.activated == true
+  
